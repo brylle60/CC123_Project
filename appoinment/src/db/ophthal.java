85700acf1847de45b5e4e576980928e41c55cdf8
P-Chel <chelsiefaith07@gmail.com>
2024-05-29 23:41:00 +0800

    db edit add the time and date

--- appoinment/src/db/ophthal.java
+++ appoinment/src/db/ophthal.java
@@ -3,12 +3,14 @@ package db;
 import constant.commonconstant;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class ophthal {
-    public static boolean register(String last_name, String first_name, String middle_name,String gender, int age, long number,String address){
+    public static boolean register(String last_name, String first_name, String middle_name, String gender, int age, long number, String address, LocalTime time_appointment, LocalDate date_appointment){
         try{
             Connection connection = DriverManager.getConnection(commonconstant.DB_USERNAME, commonconstant.DB_PASSWORD, commonconstant.DB_DOCTORS);
-            PreparedStatement insertUser = connection.prepareStatement("INSERT INTO " + commonconstant.OPHTHAL + "( last_name,first_name, middle_name, sex, age, number, address)" + "VALUES(?, ?, ?, ?, ?, ?, ?)");
+            PreparedStatement insertUser = connection.prepareStatement("INSERT INTO " + commonconstant.FAMILY_MED + "( last_name,first_name, middle_name, sex, age, number, address, time_appointment, date_appointment)" + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
             insertUser.setString(1, last_name);
             insertUser.setString(2, first_name);
@@ -18,6 +20,8 @@ public class ophthal {
             insertUser.setInt(6,age);
             insertUser.setLong(7, number);
             insertUser.setString(8, address);
+            insertUser.setTime(9, Time.valueOf(time_appointment));
+            insertUser.setDate(10, Date.valueOf(date_appointment));
             insertUser.executeUpdate();
 
         }catch (SQLException e){
