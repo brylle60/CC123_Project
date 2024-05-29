f0bc7ee21c50852762e33879a7703d215d47bba7
john <johnbryllecrodua77@gamil.com>
2024-05-29 23:03:19 +0800

    c

--- appoinment/src/doctors/Family_med.java
+++ appoinment/src/doctors/Family_med.java
@@ -7,6 +7,8 @@ import gui.home;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
